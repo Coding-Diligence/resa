30b63348571f1c1a9d8e650bea8f7d5fce4d039d
Marc-Loulembo <marcoloulembo@gmail.com>
2025-06-03 11:12:38 +0200

    1.0.3 - 03/06/2025

--- front/resa/src/app/pages/success.js
+++
@@ -1,7 +0,0 @@
-export default function Success() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-green-100">
-      <h1 className="text-2xl text-green-800">✅ Paiement réussi !</h1>
-    </div>
-  );
-}
