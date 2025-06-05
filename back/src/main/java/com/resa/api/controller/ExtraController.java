49f12ec8e3db37f3f4f8f5a1795ff57232bb64e4
Lilyan Chauveau <lilyan.chauveau@gmail.com>
2025-06-05 09:58:19 +0200

    Refact : routs apis resources

--- back/src/main/java/com/resa/api/controller/ExtraController.java
+++ back/src/main/java/com/resa/api/controller/ExtraController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/extra")
+@RequestMapping("/api/extras")
 public class ExtraController {
 
     @Autowired
