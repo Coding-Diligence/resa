0ddc5aacd803339c67b3acf8591a40a05570fe4e
Yasmina-BRV <yasminal.bravo@hotmail.com>
2025-06-05 12:30:05 +0200

    refactor pom.xml : update minor version of Spring Boot

--- back/src/main/java/com/resa/api/config/ApplicationConfig.java
+++ back/src/main/java/com/resa/api/config/ApplicationConfig.java
@@ -20,13 +20,13 @@ public class ApplicationConfig {
     private UserRepository userRepository;
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
+    AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
@@ -34,12 +34,12 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 } 
